700cf37272db3a74c269910c68f5835d5efaa20a
AimarGonzlez <aimar.mel@gmail.com>
2013-02-17 17:02:22 +0100

    AI: Added new character decisions. * now storm troopers walk, wait and shot from time to time.

--- src/com/mel/wallpaper/starWars/process/WalkersProcess.java
+++ src/com/mel/wallpaper/starWars/process/WalkersProcess.java
@@ -4,10 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 import org.andengine.util.debug.Debug;
+import org.andengine.util.math.MathUtils;
 
 import com.mel.entityframework.Game;
 import com.mel.entityframework.Process;
+import com.mel.util.MathUtil;
 import com.mel.util.Point;
+import com.mel.wallpaper.starWars.entity.InvisibleWalls;
 import com.mel.wallpaper.starWars.entity.Jumper;
 import com.mel.wallpaper.starWars.entity.Map;
 import com.mel.wallpaper.starWars.entity.Shooter;
@@ -15,12 +18,15 @@ import com.mel.wallpaper.starWars.entity.Walker;
 import com.mel.wallpaper.starWars.entity.commands.Command;
 import com.mel.wallpaper.starWars.entity.commands.MoveCommand;
 import com.mel.wallpaper.starWars.entity.commands.ShootLaserCommand;
+import com.mel.wallpaper.starWars.entity.commands.WaitCommand;
 
 public class WalkersProcess extends Process
 {
 	private Map map;
 	private Game game;
 	private List<Walker> walkers;
+	
+	private List<Shooter> shooters;
 	
 	
 	public WalkersProcess(Game game, Map map) {
@@ -36,9 +42,10 @@ public class WalkersProcess extends Process
 	public void getEntitiesFromGame(Game game) {
 		this.map = (Map)game.getEntity(Map.class);
 
+		this.shooters = (List<Shooter>) game.getEntities(Shooter.class);
+
 		this.walkers = (List<Walker>) game.getEntities(Jumper.class);
-		this.walkers.addAll(game.getEntities(Walker.class));
-		this.walkers.addAll(game.getEntities(Shooter.class));		
+		this.walkers.addAll(game.getEntities(Walker.class));	
 	}
 	
 	@Override
@@ -56,36 +63,65 @@ public class WalkersProcess extends Process
 
 		getEntitiesFromGame(game);
 		
+		
+		for(Shooter shooter : shooters){
+			if(shooter.canShoot()){
+				ShootLaserCommand laserCmd = new ShootLaserCommand(shooter,game);
+				laserCmd.destination = InvisibleWalls.getRandomPoint();
+				shooter.addCommand(laserCmd);
+			}else{
+				if(shooter.isIdle()){
+					if(Math.random()<0.5){
+						MoveCommand move = new MoveCommand(shooter);
+						move.setMovable(shooter);
+						move.destination = InvisibleWalls.getRandomPoint();
+						shooter.addCommand(move);
+					}else{
+						WaitCommand wait = new WaitCommand(shooter, MathUtils.random(2f, 4f));
+						shooter.addCommand(wait);
+					}
+				}
+			}
+		
+		}
+		
+		executeShooterCommands();
+		
+		
 		for(Walker walker : walkers) {
 			
-			if(walker.hasDestination()||walker.isBusy())
-				continue;
-			
-			MoveCommand move = new MoveCommand(walker);
-			move.setMovable(walker);
-			move.destination = map.walls.getRandomPoint();
-			walker.addCommand(move);
+			if(walker.isIdle()){
+				if(Math.random()<0.5){
+					MoveCommand move = new MoveCommand(walker);
+					move.setMovable(walker);
+					move.destination = InvisibleWalls.getRandomPoint();
+					walker.addCommand(move);
+				}else{
+					WaitCommand wait = new WaitCommand(walker, MathUtils.random(2f, 4f));
+					walker.addCommand(wait);
+				}
+			}
 		}
 	
-//		Shooter shooter = (Shooter)walkers.get(0);
-//
-//		if(!shooter.isBusy())
-//		{
-//			ShootLaserCommand laserCmd = new ShootLaserCommand(shooter,game);
-//			laserCmd.destination = map.walls.getRandomPoint();
-//			shooter.addCommand(laserCmd);
-//		}
 		
-		executeCommandsByRandomPlayer();	
+		executeWalkerCommands();	
 	}
 
-	private void executeCommandsByRandomPlayer(){
-		Collections.shuffle(this.walkers);
-		for(Walker player:this.walkers){
-			for(Command c:player.pendingCommands){
+	private void executeShooterCommands(){
+		for(Shooter shooter:this.shooters){
+			for(Command c : shooter.pendingCommands){
 				c.execute(this.map);
 			}
-			player.clearPendingCommands();
+			shooter.clearPendingCommands();
+		}
+	}
+	
+	private void executeWalkerCommands(){
+		for(Walker walker:this.walkers){
+			for(Command c : walker.pendingCommands){
+				c.execute(this.map);
+			}
+			walker.clearPendingCommands();
 		}
 	}
 	
